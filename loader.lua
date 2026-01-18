f4e9aaa7463ba8e805d40f385d8233c1a74f4c56
vixzury <hiyournicem@gmail.com>
2026-01-18 14:05:34 -0800

    Update loader.lua

--- loader.lua
+++ loader.lua
@@ -1,1 +1,1 @@
-loadstring(game:HttpGet("https://raw.githubusercontent.com/vixzury/loaderroblox/refs/heads/main/loader.luau"))()
+loadstring(game:HttpGet("https://raw.githubusercontent.com/vixzury/loaderroblox/blob/main/loader.luau"))()
